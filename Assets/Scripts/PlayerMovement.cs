72417c3376c68f238caf1182e59cfb23426b170a
Alfonso Jiménez <yo@alfonsojimenez.net>
2024-10-01 10:24:26 +0200

    Limitar salto y animación mirada

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-
+    bool isFacingRight = true;
     [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
     [Header("Jumping")]
     public float jumpPower = 10f;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2 (0.5f, 0.05f);
+    public LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        flip();
     }
 
     public void Move(InputAction.CallbackContext context) {
@@ -30,8 +36,31 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Jump(InputAction.CallbackContext context) {
-        if (context.performed) {
-            rb.velocity=new Vector2(rb.velocity.x, jumpPower);
+        if (isGrounded()) {
+            if (context.performed) {
+                rb.velocity=new Vector2(rb.velocity.x, jumpPower);
+            }
+        }
+    }
+
+    private bool isGrounded() {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) {
+            return true;
+        }
+		return false;
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+
+        void flip() {
+        if ((isFacingRight && horizontalMovement < 0) || (!isFacingRight && horizontalMovement > 0)) {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
         }
     }
 }
